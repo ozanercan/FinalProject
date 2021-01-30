da3486c9e7841d24c47292ab801be412b8ca6a14
Ozan ERCAN <ozanercan07@outlook.com>
2021-01-31 00:05:24 +0300

    Add project files.

---
+++ Presentations/ConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IProductService productService = new ProductManager(new InMemoryProductDal());
+
+            var productList = productService.GetAll();
+
+            productList.ForEach(p => Console.WriteLine(p.ProductName));
+
+            Console.ReadLine();
+        }
+    }
+}
