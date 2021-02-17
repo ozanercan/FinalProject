886eb810068b7059076c6fa07d471302d2fee6ba
Ozan ERCAN <ozanercan07@outlook.com>
2021-02-18 00:11:41 +0300

    12. gün gün sonu değişiklikleri Fluent Validation, Fluent Api, AuoFac eklendi. Aspect alt yapısı oluşturuldu ve test edildi.

---
+++ Libraries/Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Business.Abstract;
+using Business.Concrete;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<EfProductDal>().As<IProductDal>();
+            builder.RegisterType<EfOrderDal>().As<IOrderDal>();
+            builder.RegisterType<EfCategoryDal>().As<ICategoryDal>();
+
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
+        }
+    }
+}
